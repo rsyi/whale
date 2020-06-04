37c68d816a5bd0811839d91f4c62a1ecee708bbc
Robert Yi <robert@ryi.me>
2020-06-04 07:46:56 -0700

    feat: enable all flags.

--- tests/unit/extractor/test_presto_loop_extractor.py
+++ tests/unit/extractor/test_presto_loop_extractor.py
@@ -11,23 +11,21 @@ from metaframe.engine.sql_alchemy_engine import SQLAlchemyEngine
 
 class TestPrestoLoopExtractor(unittest.TestCase):
     def setUp(self):
-        # type: () -> None
         logging.basicConfig(level=logging.INFO)
 
         config_dict = {
             'conn_string': 'TEST_CONNECTION',
-            'is_table_metadata_enabled': False,
-            'is_full_extraction_enabled': False,
-            'is_watermark_enabled': False,
-            'is_stats_enabled': False,
-            'is_analyze_enabled': False
+            'is_table_metadata_enabled': True,
+            'is_full_extraction_enabled': True,
+            'is_watermark_enabled': True,
+            'is_stats_enabled': True,
+            'is_analyze_enabled': True
         }
         self.conf = ConfigFactory.from_dict(config_dict)
 
     def test_extraction_with_empty_result(self):
-        # type: () -> None
         """
-        Test Extraction with empty result from query
+        Test Extraction with empty result from query.
         """
         with patch.object(SQLAlchemyEngine, '_get_connection'):
             extractor = PrestoLoopExtractor()
