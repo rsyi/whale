a9b0294f4ccee87f4ec1df3c59f9ae821118373f
Robert Yi <robert@ryi.me>
2020-09-17 14:08:38 -0700

    feat: add onboarding process in rust.

--- whale-cli/src/main.rs
+++ whale-cli/src/main.rs
@@ -1,13 +1,28 @@
 extern crate clap;
 
-mod skimmer;
-
-use clap::{App, Arg};
+use whale;
+use clap::{App, SubCommand};
 
 fn main() {
-    let matches = App::new("wh")
-        .arg(Arg::with_name("init"))
-        .get_matches();
+    let app = App::new("Whale CLI")
+        .about("Data WareHouse And Lake Explorer")
+        .author("Robert Yi (@rsyi on github)")
+        .subcommand(SubCommand::with_name("init")
+            .about("Initializes the scheduler and extraction pipeline"))
+        .subcommand(SubCommand::with_name("etl")
+            .about("Manually runs the metadata extraction job"));
 
-    skimmer::table_skim();
+    let matches = app.get_matches();
+
+    if let Some(init_matches) = matches.subcommand_matches("init") {
+        let output = whale::Whale::init();
+    }
+
+    else if let Some(etl_matches) = matches.subcommand_matches("etl") {
+        let output = whale::Whale::etl();
+    }
+
+    else {
+        let output = whale::Whale::run_with(matches);
+    }
 }
