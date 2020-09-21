798ad95f1c5cdf98687c06a03c1d3d7106bcfc34
Robert Yi <robert@ryi.me>
2020-09-21 10:40:24 -0700

    feat: use manifest from static whale dir

--- cli/src/skimmer.rs
+++ cli/src/skimmer.rs
@@ -2,13 +2,17 @@ extern crate skim;
 
 use skim::prelude::{Skim, SkimItemReader, SkimOptionsBuilder};
 use std::fs;
+use std::path::Path;
 use std::io::Cursor;
-
-const MANIFEST_FILE_PATH: &str = "./short_table_manifest.txt";
 
 pub fn table_skim() {
 
-    let table_manifest = fs::read_to_string(MANIFEST_FILE_PATH)
+    let base_path = shellexpand::tilde("~");
+    let base_path = Path::new(&*base_path);
+    let whale_base_path = base_path.join(".whale");
+    let manifest_file_path = whale_base_path.join("short_table_manifest.txt");
+
+    let table_manifest = fs::read_to_string(manifest_file_path)
         .expect("Failed to read file.");
 
     println!("Manifest: {}", table_manifest);
