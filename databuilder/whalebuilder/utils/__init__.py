aac802aa9d707f563e0dcd44da3053973e5eac24
Robert Yi <robert@ryi.me>
2020-09-22 14:24:58 -0700

    feat: fully functioning markdown excision functions

--- databuilder/whalebuilder/utils/__init__.py
+++ databuilder/whalebuilder/utils/__init__.py
@@ -1,5 +1,7 @@
 import os
 from pathlib import Path
+from whalebuilder.transformer.markdown_transformer import FormatterMixin
+from whalebuilder.utils.markdown_delimiters import UGC_DELIMITER
 
 TABLE_RELATIVE_FILE_PATH = '{database}/{cluster}.{schema}.{table}'
 CLUSTERLESS_TABLE_RELATIVE_FILE_PATH = '{database}/{schema}.{table}'
@@ -30,6 +32,19 @@ def get_table_file_path_base(
     return os.path.join(base_directory, relative_file_path)
 
 
+def create_base_table_stub(
+        file_path,
+        database,
+        cluster,
+        schema,
+        table,
+    ):
+    text_to_write = f"# `{schema}.{table}`\n{database} | {cluster}\n" \
+        + UGC_DELIMITER \
+        + "\n*Edits above this line will be overwritten.*\n"
+    safe_write(file_path, text_to_write)
+
+
 def safe_write(file_path_to_write, text_to_write, tmp_extension=".bak"):
     backup_file_path = file_path_to_write + tmp_extension
 
@@ -39,6 +54,5 @@ def safe_write(file_path_to_write, text_to_write, tmp_extension=".bak"):
         os.fsync(f.fileno())
 
     os.rename(backup_file_path, file_path_to_write)
-
 
 
