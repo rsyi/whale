4ecc2cde7d07049988879f8a265b00ab21840625
Robert Yi <robert@ryi.me>
2020-09-29 21:29:06 -0700

    chore: comment out dashboard for now.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -7,11 +7,10 @@ fn main() {
         .author("Robert Yi (@rsyi on github)")
         .subcommand(SubCommand::with_name("init")
             .about("Initializes the scheduler and extraction pipeline"))
-        .subcommand(SubCommand::with_name("dash")
-            .about("Show macro-level dashboard of metadata metadata"))
+        //.subcommand(SubCommand::with_name("dash")
+        //    .about("Show macro-level dashboard of metadata metadata"))
         .subcommand(SubCommand::with_name("connections")
-            .about("Access warehouse connection information")
-        )
+            .about("Access warehouse connection information"))
         .subcommand(SubCommand::with_name("etl")
             .about("Manually runs the metadata extraction job"))
         .subcommand(SubCommand::with_name("schedule")
@@ -21,7 +20,7 @@ fn main() {
 
     match matches.subcommand_name() {
         Some("init") => whale::Whale::init(),
-        Some("dash") => whale::Whale::dash(),
+        // Some("dash") => whale::Whale::dash(),
         Some("etl") => whale::Whale::etl(),
         Some("schedule") => whale::Whale::schedule(true),
         Some("connections") => whale::Whale::connections(),
--- cli/src/tuievent.rs
+++ cli/src/tuievent.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 use std::io;
 use std::sync::mpsc;
 use std::sync::{
