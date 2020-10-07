8601ccd89ef368d3b5011d7ec9f3626a3bfce2db
Robert Yi <robert@ryi.me>
2020-10-07 12:51:37 -0700

    feat: add wh config command

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -9,8 +9,10 @@ fn main() {
             .about("Initializes the scheduler and extraction pipeline"))
         //.subcommand(SubCommand::with_name("dash")
         //    .about("Show macro-level dashboard of metadata metadata"))
+        .subcommand(SubCommand::with_name("config")
+            .about("Open file containing whale config information"))
         .subcommand(SubCommand::with_name("connections")
-            .about("Access warehouse connection information"))
+            .about("Open file containing warehouse connection information"))
         .subcommand(SubCommand::with_name("git-enable")
             .about("Enable git"))
         .subcommand(SubCommand::with_name("etl")
@@ -25,6 +27,7 @@ fn main() {
         // Some("dash") => whale::Whale::dash(),
         Some("etl") => whale::Whale::etl(),
         Some("schedule") => whale::Whale::schedule(true),
+        Some("config") => whale::Whale::config(),
         Some("connections") => whale::Whale::connections(),
         Some("git-enable") => whale::Whale::git_enable(),
         _ => whale::Whale::run_with(matches)
