08723d80449e134abe9402ab2b69ea3593c3b4ed
rsyi <rosiny@gmail.com>
2020-10-16 19:03:04 -0700

    fix: broken test when there are no connections

--- databuilder/tests/unit/utils/test_task_wrappers.py
+++ databuilder/tests/unit/utils/test_task_wrappers.py
@@ -5,11 +5,16 @@ from whalebuilder.engine.sql_alchemy_engine import SQLAlchemyEngine
 from whalebuilder.utils.task_wrappers import run, pull
 from ..fixtures import mock_file_structure
 from whalebuilder.utils import paths
+from whalebuilder.utils import task_wrappers
 
 
-@patch.object(SQLAlchemyEngine, "execute",)
-def test_run(mock_execution):
+@patch.object(task_wrappers, "get_connection")
+@patch.object(SQLAlchemyEngine, "execute")
+def test_run(mock_execution, get_connection):
     mock_execution.return_value = iter([["hi"], [0]])
+    get_connection.return_value = {
+        "metadata_source": "Bigquery",
+    }
     sql = "select count(anonymous_id) from `bigquery-sample-no-prod-stuff`.census.button_clicked;"
     df = run(sql)
     assert type(df) == pd.DataFrame
