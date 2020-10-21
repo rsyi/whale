rsyi/whale

commit 7cc42add0859459139fd3301204746ce89a739cd

chore: run cargo format (#45)
* chore: run cargo format

* chore: fix clippy warnings

* using format with no args - https://rust-lang.github.io/rust-clippy/master/index.html\#print_literal
* use is_empty rather than length checks
* use is_some rather than matching
* other minor changes to keep clippy happy

* fix: add negation to contains to get desired bool returned

cli/src/lib.rs
@@ -1,39 +1,32 @@
 mod tuievent;
 
-#[macro_use] extern crate lazy_static;
-use clap::{ArgMatches};
+#[macro_use]
+extern crate lazy_static;
+use crate::tuievent::{Event, Events};
+use clap::ArgMatches;
 use colored::*;
 use std::{
     io::{self, Write},
     process::Command,
 };
+use termion::{clear, event::Key, raw::IntoRawMode};
 use tui::{
     backend::TermionBackend,
-    layout::{Layout, Constraint, Direction},
+    layout::{Constraint, Direction, Layout},
     style::{Color, Modifier, Style},
     symbols,
     text::Span,
     widgets::{Axis, Block, Borders, Chart, Dataset, GraphType},
     Terminal,
 };
-use termion::{
-    clear,
-    event::Key,
-    raw::IntoRawMode,
-};
-use crate::tuievent::{
-    {Event, Events},
-};
 
-pub mod warehouse;
-pub mod skimmer;
-pub mod utils;
 pub mod filesystem;
 pub mod serialization;
-
+pub mod skimmer;
+pub mod utils;
+pub mod warehouse;
 
 const DEFAULT_CRON_STRING: &str = "0 */6 * * *";
-
 
 fn print_initialization_header() {
     let whale_header = "
@@ -45,26 +38,30 @@ oooo oooo    ooo  888 .oo.          :
    `888'`888'     888   888    |  o        \\___/  |
     `8'  `8'     o888o o888o  ~^~^~^~^~^~^~^~^~^~^~^~
 
-    Data Warehouse Command-Line Explorer".cyan().bold();
+    Data Warehouse Command-Line Explorer"
+        .cyan()
+        .bold();
 
     println!("{}", whale_header);
     // println!("
-// Whale needs to initialize the following file structure:
-//
-//  ~/.whale
-//  ├── config
-//  │   └── connections.yaml
-//  ├── logs
-//  └── metadata
-//
-// We'll check if this exists and make it if it doesn't.")
+    // Whale needs to initialize the following file structure:
+    //
+    //  ~/.whale
+    //  ├── config
+    //  │   └── connections.yaml
+    //  ├── logs
+    //  └── metadata
+    //
+    // We'll check if this exists and make it if it doesn't.")
 }
-
 
 fn print_scheduler_header() {
-    println!("\n{} [Default: {} (on the hour, every 6 hours)]", "Enter cron string.".purple(), DEFAULT_CRON_STRING.green());
+    println!(
+        "\n{} [Default: {} (on the hour, every 6 hours)]",
+        "Enter cron string.".purple(),
+        DEFAULT_CRON_STRING.green()
+    );
 }
-
 
 /// Holds all command-line tasks.
 ///
@@ -80,11 +77,12 @@ impl Whale {
             .unwrap();
         if is_git_etl_enabled {
             let whale_dirname = filesystem::get_base_dirname();
-            let command = format!("cd {} && (git status | grep Unmerged > /dev/null 2>&1 && echo true || echo false)", whale_dirname);
+            let command = format!(
+                "cd {} && (git status | grep Unmerged > /dev/null 2>&1 && echo true || echo false)",
+                whale_dirname
+            );
 
-            let output = Command::new("sh")
-                .args(&["-c", &command])
-                .output()?;
+            let output = Command::new("sh").args(&["-c", &command]).output()?;
             let is_unmerged_files_found = String::from_utf8_lossy(&output.stdout)
                 .trim()
                 .parse()
@@ -101,12 +99,16 @@ impl Whale {
         filesystem::create_file_structure();
 
         if !utils::is_cron_expression_registered() {
-            println!("\n{} [{}/{}]",
-                     "Would you like to register a cron job to periodically scrape metadata?".purple(),
-                     "Y".green(),
-                     "n".red()
-                     );
-            println!("This is resource-light and can be manually deleted by editing the file at {}.", "crontab -e".magenta());
+            println!(
+                "\n{} [{}/{}]",
+                "Would you like to register a cron job to periodically scrape metadata?".purple(),
+                "Y".green(),
+                "n".red()
+            );
+            println!(
+                "This is resource-light and can be manually deleted by editing the file at {}.",
+                "crontab -e".magenta()
+            );
             let is_scheduling_requested: bool = utils::get_input_as_bool();
             if is_scheduling_requested {
                 Whale::schedule(false)?;
@@ -129,28 +131,30 @@ impl Whale {
         if git_address == "None" {
             println!("You must supply a git remote.");
             Ok(())
-        }
-        else {
-            let init_command = format!("cd ~/.whale && git init && git remote add origin {}", git_address);
+        } else {
+            let init_command = format!(
+                "cd ~/.whale && git init && git remote add origin {}",
+                git_address
+            );
             let gitignore_command = "echo 'bin/\nlogs/\nconfig/config.yaml\nlibexec/' > .gitignore";
-            let git_push_command = "git add . && git commit -m 'Whale on our way' && git push -u origin master";
-            let full_command = format!("{} && {} && {}", init_command, gitignore_command, git_push_command);
-            let mut child = Command::new("sh")
-                .args(&["-c", &full_command])
-                .spawn()?;
+            let git_push_command =
+                "git add . && git commit -m 'Whale on our way' && git push -u origin master";
+            let full_command = format!(
+                "{} && {} && {}",
+                init_command, gitignore_command, git_push_command
+            );
+            let mut child = Command::new("sh").args(&["-c", &full_command]).spawn()?;
             child.wait()?;
 
             Ok(())
         }
     }
 
-    pub fn config() -> Result<(), io::Error>{
+    pub fn config() -> Result<(), io::Error> {
         let config_file = filesystem::get_config_filename();
         let editor = filesystem::get_open_command();
 
-        Command::new(editor)
-            .arg(config_file)
-            .status()?;
+        Command::new(editor).arg(config_file).status()?;
 
         Ok(())
     }
@@ -159,9 +163,7 @@ impl Whale {
         let connections_config_file = filesystem::get_connections_filename();
         let editor = filesystem::get_open_command();
 
-        Command::new(editor)
-            .arg(connections_config_file)
-            .status()?;
+        Command::new(editor).arg(connections_config_file).status()?;
 
         Ok(())
     }
@@ -178,21 +180,19 @@ Fetching and rebasing local changes from github.");
             let whale_dirname = filesystem::get_base_dirname();
 
             let command = format!("cd {} && git pull --rebase --autostash", whale_dirname);
-            let mut child = Command::new("sh")
-                .args(&["-c", &command])
-                .spawn()?;
+            let mut child = Command::new("sh").args(&["-c", &command]).spawn()?;
             child.wait()?;
-        }
-        else {
+        } else {
             // let build_script_path = filesystem::get_build_script_filename();
             // let build_script_path = Path::new(&*build_script_path);
             let python_alias = serialization::read_config("python3_alias", "python3");
             let activate_path = filesystem::get_activate_filename();
             let build_script_path = filesystem::get_build_script_filename();
-            let full_command = format!("source {} && {} {}", activate_path, python_alias, build_script_path);
-            let mut child = Command::new("sh")
-                .args(&["-c", &full_command])
-                .spawn()?;
+            let full_command = format!(
+                "source {} && {} {}",
+                activate_path, python_alias, build_script_path
+            );
+            let mut child = Command::new("sh").args(&["-c", &full_command]).spawn()?;
             child.wait()?;
 
             let manifest_path = filesystem::get_manifest_filename();
@@ -202,16 +202,16 @@ Fetching and rebasing local changes from github.");
         Ok(())
     }
 
-
-    pub fn run(sql_file: &str, warehouse_name: &str) -> Result<(), io::Error>{
+    pub fn run(sql_file: &str, warehouse_name: &str) -> Result<(), io::Error> {
         let python_alias = serialization::read_config("python3_alias", "python3");
         let activate_path = filesystem::get_activate_filename();
         let run_script_path = filesystem::get_run_script_filename();
         let arguments = format!("{} {}", sql_file, warehouse_name);
-        let full_command = format!("source {} && {} {} {}", activate_path, python_alias, run_script_path, arguments);
-        let mut child = Command::new("sh")
-            .args(&["-c", &full_command])
-            .spawn()?;
+        let full_command = format!(
+            "source {} && {} {} {}",
+            activate_path, python_alias, run_script_path, arguments
+        );
+        let mut child = Command::new("sh").args(&["-c", &full_command]).spawn()?;
         child.wait()?;
 
         Ok(())
@@ -226,25 +226,29 @@ Fetching and rebasing local changes from github.");
         let is_valid_cron: bool = utils::is_valid_cron_expression(&cron_string);
         if is_valid_cron {
             println!(
-                "\n{} {}.\n",
-                "Valid cron string detected:",
-                cron_string.yellow());
-        }
-        else {
+                "\nValid cron string detected: {}.\n",
+                cron_string.yellow()
+            );
+        } else {
             println!(
-                "\n{} {} {} {}\n",
+                "\n{} Cron expression {} appears invalid. Proceed with caution\n",
                 "WARNING:".bold().red(),
-                "Cron expression",
                 cron_string.yellow(),
-                "appears invalid. Proceed with caution.",
-                );
+            );
         }
 
         let mut can_add_crontab = true;
         if ask_for_permission {
-            println!("{} [{}, {}]", "Register metadata scraping job at this schedule in crontab?".purple(), "Y".green(), "n".red());
+            println!(
+                "{} [{}, {}]",
+                "Register metadata scraping job at this schedule in crontab?".purple(),
+                "Y".green(),
+                "n".red()
+            );
             println!("This will excise and replace any whale-based cron job you've already registered through this interface.");
-            println!("You can manually delete this later by editing the file accessed by `crontab -e`.");
+            println!(
+                "You can manually delete this later by editing the file accessed by `crontab -e`."
+            );
 
             can_add_crontab = utils::get_input_as_bool();
         }
@@ -252,36 +256,35 @@ Fetching and rebasing local changes from github.");
         if can_add_crontab {
             let whale_etl_command = filesystem::get_etl_command();
 
-            let whale_cron_expression = format!(
-                "{} {}",
-                cron_string,
-                whale_etl_command);
-            let scheduler_command = format!("(crontab -l | fgrep -v \"{}\"; echo \"{}\") | crontab -", whale_etl_command, whale_cron_expression);
+            let whale_cron_expression = format!("{} {}", cron_string, whale_etl_command);
+            let scheduler_command = format!(
+                "(crontab -l | fgrep -v \"{}\"; echo \"{}\") | crontab -",
+                whale_etl_command, whale_cron_expression
+            );
 
             Command::new("sh")
                 .args(&["-c", &scheduler_command])
                 .spawn()?;
 
-            println!("If you are prompted for permissions, allow and continue. [Press {} to continue]", "enter".green());
+            println!(
+                "If you are prompted for permissions, allow and continue. [Press {} to continue]",
+                "enter".green()
+            );
             utils::get_input();
 
             if utils::is_cron_expression_registered() {
                 println!("{}", "Cron expression successfully registered.".cyan());
-            }
-            else {
-                println!("{} {}", "WARNING:".red(), "Cron tab failed to register.");
+            } else {
+                println!("{} Cron tab failed to register.", "WARNING:".red());
                 println!("If failures persist, manually add the following line to your crontab (accessed through {}).", "crontab -e".magenta());
                 println!("{}", whale_cron_expression.magenta());
             }
-
         }
 
         Ok(())
     }
 
-
     pub fn dash() -> Result<(), io::Error> {
-
         const DATA2: [(f64, f64); 7] = [
             (0.0, 0.0),
             (10.0, 1.0),
@@ -303,17 +306,10 @@ Fetching and rebasing local changes from github.");
                 let chunks = Layout::default()
                     .direction(Direction::Vertical)
                     .margin(1)
-                    .constraints(
-                        [
-                            Constraint::Percentage(50),
-                            Constraint::Percentage(50),
-                        ].as_ref()
-                    )
+                    .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
                     .split(f.size());
 
-                let block = Block::default()
-                     .title("Block")
-                     .borders(Borders::ALL);
+                let block = Block::default().title("Block").borders(Borders::ALL);
 
                 let datasets = vec![Dataset::default()
                     .name("count_distinct(nulls)")
@@ -327,7 +323,7 @@ Fetching and rebasing local changes from github.");
                         Axis::default()
                             .title("X Axis")
                             .style(Style::default().fg(Color::Gray))
-                            .bounds([0.0, 60.0])
+                            .bounds([0.0, 60.0]),
                     )
                     .y_axis(
                         Axis::default()
@@ -337,14 +333,12 @@ Fetching and rebasing local changes from github.");
                                 Span::styled("3", Style::default().add_modifier(Modifier::BOLD)),
                                 Span::raw("0"),
                             ])
-                            .bounds([0.0, 3.0])
+                            .bounds([0.0, 3.0]),
                     );
 
                 f.render_widget(chart, chunks[0]);
 
-                let block = Block::default()
-                     .title("Block 2")
-                     .borders(Borders::ALL);
+                let block = Block::default().title("Block 2").borders(Borders::ALL);
                 f.render_widget(block, chunks[1]);
             })?;
 
@@ -354,10 +348,10 @@ Fetching and rebasing local changes from github.");
                     if input == Key::Char('q') {
                         break;
                     }
-                },
-                Event::Tick => ()
+                }
+                Event::Tick => (),
             };
-        };
+        }
 
         Ok(())
     }

cli/src/main.rs
@@ -1,42 +1,59 @@
-use whale;
 use clap::{App, Arg, SubCommand};
+
 
 fn main() {
     let app = App::new("Whale CLI")
         .about("Data WareHouse And Lake Explorer")
         .author("Robert Yi (@rsyi on github)")
-        .subcommand(SubCommand::with_name("init")
-            .about("Initializes the scheduler and extraction pipeline"))
+        .subcommand(
+            SubCommand::with_name("init")
+                .about("Initializes the scheduler and extraction pipeline"),
+        )
         //.subcommand(SubCommand::with_name("dash")
         //    .about("Show macro-level dashboard of metadata metadata"))
-        .subcommand(SubCommand::with_name("config")
-            .about("Open file containing whale config information"))
-        .subcommand(SubCommand::with_name("connections")
-            .about("Open file containing warehouse connection information"))
-        .subcommand(SubCommand::with_name("git-enable")
-            .about("Enable git"))
-        .subcommand(SubCommand::with_name("git-setup")
-            .about("Set up ~/.whale directory for git and run initial push")
-            .arg(Arg::with_name("remote")
-                 .help("Remote git address where whale will be backed up"))
-            )
-        .subcommand(SubCommand::with_name("etl")
-            .about("Manually runs the metadata extraction job (deprecated: use `wh pull` instead)"))
-        .subcommand(SubCommand::with_name("pull")
-            .about("Manually runs the metadata extraction job"))
-        .subcommand(SubCommand::with_name("run")
-            .about("Execute a query file")
-            .arg(Arg::with_name("sql_file")
-                 .help("File path that contains the sql file to run")
-                 .required(true))
-            .arg(Arg::with_name("warehouse_name")
-                 .short("w")
-                 .long("warehouse")
-                 .help("Warehouse name to run the query against")
-                 .required(false))
-            )
-        .subcommand(SubCommand::with_name("schedule")
-            .about("Register metadata scraping job with crontab"));
+        .subcommand(
+            SubCommand::with_name("config").about("Open file containing whale config information"),
+        )
+        .subcommand(
+            SubCommand::with_name("connections")
+                .about("Open file containing warehouse connection information"),
+        )
+        .subcommand(SubCommand::with_name("git-enable").about("Enable git"))
+        .subcommand(
+            SubCommand::with_name("git-setup")
+                .about("Set up ~/.whale directory for git and run initial push")
+                .arg(
+                    Arg::with_name("remote")
+                        .help("Remote git address where whale will be backed up"),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("etl").about(
+                "Manually runs the metadata extraction job (deprecated: use `wh pull` instead)",
+            ),
+        )
+        .subcommand(
+            SubCommand::with_name("pull").about("Manually runs the metadata extraction job"),
+        )
+        .subcommand(
+            SubCommand::with_name("run")
+                .about("Execute a query file")
+                .arg(
+                    Arg::with_name("sql_file")
+                        .help("File path that contains the sql file to run")
+                        .required(true),
+                )
+                .arg(
+                    Arg::with_name("warehouse_name")
+                        .short("w")
+                        .long("warehouse")
+                        .help("Warehouse name to run the query against")
+                        .required(false),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("schedule").about("Register metadata scraping job with crontab"),
+        );
 
     let matches = app.get_matches();
 
@@ -69,7 +86,7 @@ fn main() {
         Some("git-enable") => whale::Whale::git_enable(),
         Some("git-setup") => whale::Whale::git_setup(git_address),
         Some("run") => whale::Whale::run(sql_file, warehouse_name),
-        _ => whale::Whale::run_with(matches)
-    }.expect("Failed to run command.");
-
+        _ => whale::Whale::run_with(matches),
+    }
+    .expect("Failed to run command.");
 }

cli/src/skimmer.rs
@@ -1,14 +1,13 @@
+use crate::filesystem;
+use crate::serialization;
+use crate::utils;
 use skim::prelude::{Skim, SkimItemReader, SkimOptionsBuilder};
 use std::fs;
+use std::io::Cursor;
 use std::path::Path;
 use std::process::Command;
-use std::io::Cursor;
-use crate::filesystem;
-use crate::utils;
-use crate::serialization;
 
 pub fn table_skim() {
-
     let manifest_file_path = filesystem::get_manifest_filename();
     let tmp_manifest_file_path = filesystem::get_tmp_manifest_filename();
 
@@ -17,16 +16,13 @@ pub fn table_skim() {
     let table_manifest;
     let is_manifest_found: bool;
     if Path::new(&manifest_file_path).exists() {
-        table_manifest = fs::read_to_string(manifest_file_path)
-            .expect("Failed to read manifest.");
+        table_manifest = fs::read_to_string(manifest_file_path).expect("Failed to read manifest.");
         is_manifest_found = true;
-    }
-    else if Path::new(&tmp_manifest_file_path).exists() {
+    } else if Path::new(&tmp_manifest_file_path).exists() {
         table_manifest = fs::read_to_string(tmp_manifest_file_path)
             .expect("Failed to read manifest or tmp manifest.");
         is_manifest_found = true;
-    }
-    else {
+    } else {
         table_manifest = "No tables found.".to_string();
         is_manifest_found = false;
     }
@@ -35,8 +31,7 @@ pub fn table_skim() {
     let metadata_path = filesystem::get_metadata_dirname();
     if is_manifest_found {
         preview_arg = format!("{}/{}.md", metadata_path, "{}");
-    }
-    else {
+    } else {
         preview_arg = "<< EOF
     ___=____
   ,^        ^.      _
@@ -57,7 +52,8 @@ pub fn table_skim() {
 
  Good luck, and happy whaling!
 
-        ".to_string();
+        "
+        .to_string();
     }
 
     let reader;
@@ -65,7 +61,7 @@ pub fn table_skim() {
     let custom_preview_command = serialization::read_config("preview_command", "");
 
     if custom_preview_command.is_empty() {
-        let bat_testing_command = format!("command -v bat > /dev/null 2>&1 && echo true || echo false");
+        let bat_testing_command = "command -v bat > /dev/null 2>&1 && echo true || echo false".to_string();
         let output = Command::new("sh")
             .args(&["-c", &bat_testing_command])
             .output()
@@ -77,12 +73,10 @@ pub fn table_skim() {
 
         if is_bat_installed {
             reader = "bat --color=always --style='changes'";
-        }
-        else {
+        } else {
             reader = "cat";
         }
-    }
-    else {
+    } else {
         reader = &custom_preview_command;
     }
 
@@ -100,7 +94,7 @@ pub fn table_skim() {
     // `run_with` would read and show items from the stream
     let selected_items = Skim::run_with(&options, Some(items))
         .map(|out| out.selected_items)
-        .unwrap_or_else(|| Vec::new());
+        .unwrap_or_else(Vec::new);
 
     // let recently_used_filename = filesystem::get_recently_used_filename();
     for item in selected_items.iter() {
@@ -118,5 +112,4 @@ pub fn table_skim() {
     }
 
     // filesystem::deduplicate_file(&recently_used_filename);
-
 }

commit 42935540ece1de21f8d6b269df382202b5fe5b1a

chore: Add Cargo Github Workflow
* rerun cargo fmt to fix issues introduced in
https://github.com/biomunky/whale/commit/7cc42add0859459139fd3301204746ce89a739cd,
required to make workflow pass
* add workflow that runs fmt, clippy, check and tests

.github/workflows/cargo-ci.yml
@@ -1,0 +1,87 @@
+---
+# shamelessly lifted from https://github.com/actions-rs/meta/blob/master/recipes/quickstart.md
+
+on:
+  - push
+  - pull_request
+
+name: CI
+
+jobs:
+  fmt:
+    name: Rustfmt
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v1
+      - uses: actions-rs/toolchain@v1
+        with:
+          profile: minimal
+          toolchain: stable
+          override: true
+      - run: rustup component add rustfmt
+      - uses: actions-rs/cargo@v1
+        with:
+          command: fmt
+          args: --manifest-path ./cli/Cargo.toml --all -- --check
+
+  clippy:
+    name: Clippy
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v1
+      - uses: actions-rs/toolchain@v1
+        with:
+          profile: minimal
+          toolchain: stable
+          override: true
+      - run: rustup component add clippy
+      - uses: actions-rs/cargo@v1
+        with:
+          command: clippy
+          args: --manifest-path ./cli/Cargo.toml -- -D warnings
+
+  check:
+    name: Check
+    runs-on: ${{ matrix.os }}
+
+    strategy:
+      fail-fast: false
+
+      matrix:
+        os:
+          - ubuntu-latest
+          - macos-latest
+    steps:
+      - uses: actions/checkout@v1
+      - uses: actions-rs/toolchain@v1
+        with:
+          profile: minimal
+          toolchain: stable
+          override: true
+      - uses: actions-rs/cargo@v1
+        with:
+          command: check
+          args: --manifest-path ./cli/Cargo.toml
+
+  test:
+    name: Test Suite
+    runs-on: ${{ matrix.os }}
+
+    strategy:
+      fail-fast: false
+
+      matrix:
+        os:
+          - ubuntu-latest
+          - macos-latest
+    steps:
+      - uses: actions/checkout@v1
+      - uses: actions-rs/toolchain@v1
+        with:
+          profile: minimal
+          toolchain: stable
+          override: true
+      - uses: actions-rs/cargo@v1
+        with:
+          command: test
+          args: --manifest-path ./cli/Cargo.toml

cli/src/lib.rs
@@ -225,10 +225,7 @@ Fetching and rebasing local changes from github.");
         }
         let is_valid_cron: bool = utils::is_valid_cron_expression(&cron_string);
         if is_valid_cron {
-            println!(
-                "\nValid cron string detected: {}.\n",
-                cron_string.yellow()
-            );
+            println!("\nValid cron string detected: {}.\n", cron_string.yellow());
         } else {
             println!(
                 "\n{} Cron expression {} appears invalid. Proceed with caution\n",

cli/src/main.rs
@@ -1,6 +1,5 @@
 use clap::{App, Arg, SubCommand};
 
-
 fn main() {
     let app = App::new("Whale CLI")
         .about("Data WareHouse And Lake Explorer")

cli/src/skimmer.rs
@@ -61,7 +61,8 @@ pub fn table_skim() {
     let custom_preview_command = serialization::read_config("preview_command", "");
 
     if custom_preview_command.is_empty() {
-        let bat_testing_command = "command -v bat > /dev/null 2>&1 && echo true || echo false".to_string();
+        let bat_testing_command =
+            "command -v bat > /dev/null 2>&1 && echo true || echo false".to_string();
         let output = Command::new("sh")
             .args(&["-c", &bat_testing_command])
             .output()

cli/src/warehouse.rs
@@ -82,7 +82,7 @@ pub fn prompt_add_warehouse(is_first_time: bool) {
         );
         let has_warehouse_to_add: bool = utils::get_input_as_bool();
         if !has_warehouse_to_add {
-            return
+            return;
         }
     }
 
@@ -264,9 +264,7 @@ impl GenericWarehouse {
 
         // port
         let port_msg = "Port?";
-        let port_str: String = utils::get_input_with_message(port_msg)
-            .trim()
-            .to_string();
+        let port_str: String = utils::get_input_with_message(port_msg).trim().to_string();
         let port: i32 = port_str.parse::<i32>().unwrap();
 
         // username
@@ -349,9 +347,7 @@ impl HiveMetastore {
 
         // port
         let port_msg = "Port?";
-        let port_str: String = utils::get_input_with_message(port_msg)
-            .trim()
-            .to_string();
+        let port_str: String = utils::get_input_with_message(port_msg).trim().to_string();
         let port: i32 = port_str.parse::<i32>().unwrap();
 
         // username