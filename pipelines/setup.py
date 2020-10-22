9e7fc3b7d53917e6c2a8f23d4c5dd625cf1b8734
rsyi <rosiny@gmail.com>
2020-10-22 13:14:40 -0700

    chore: advance pypi version.

--- pipelines/setup.py
+++ pipelines/setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setuptools.setup(
     name='whale-pipelines',
-    version='1.0.0',
+    version='1.1.0',
     author='Robert Yi',
     author_email='robert@ryi.me',
     description="A pared-down metadata scraper + SQL runner.",
