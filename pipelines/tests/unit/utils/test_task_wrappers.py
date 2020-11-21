2df48626ba753aabdafb8a0d8bb15c1d39896726
rsyi <rosiny@gmail.com>
2020-11-21 00:19:08 -0800

    chore: fix broken test

--- pipelines/tests/unit/utils/test_task_wrappers.py
+++ pipelines/tests/unit/utils/test_task_wrappers.py
@@ -8,10 +8,15 @@ from whale.utils import paths
 from whale.utils import task_wrappers
 
 
+def no_op_template(query):
+    return query
+
+
 @patch("google.auth.default", lambda scopes: ["dummy", "dummy"])
+@patch.object(task_wrappers, "template_query")
 @patch.object(task_wrappers, "get_connection")
 @patch.object(SQLAlchemyEngine, "execute")
-def test_run(mock_execution, get_connection):
+def test_run(mock_execution, get_connection, mock_template_query):
     mock_execution.return_value = iter([["hi"], [0]])
     get_connection.return_value = {
         "metadata_source": "bigquery",
